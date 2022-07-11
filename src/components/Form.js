59bfd0f381730429ce0e840f55415d63eb2cbb65
Warren Pope <warrenpope2010@gmail.com>
2022-07-11 22:41:25 +0530

    Updated files

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
-import { Outlet, useParams } from "react-router-dom";
+import { Outlet, useParams, useNavigate } from "react-router-dom";
 export const Form = () => {
     let { cityName } = useParams()
     if(cityName === undefined)
         cityName = ""
     const [city, setCity] = useState(cityName)
+    let navigate = useNavigate()
+    const handleSubmit = (event) =>{
+        event.preventDefault()
+        navigate(`/${city}`, { replace: false })
+        navigate(0)
+    }
     return (
         <>
-        <form onSubmit={(event) =>{ event.preventDefault(); window.location.href=`/${city}`;}} className='search-bar mt-3'>
+        <form onSubmit={handleSubmit} className='search-bar mt-3'>
             <input value={city} onChange={(event) => setCity(event.target.value)} type='text' placeholder='Enter Location...' className='form-control text-center' />
             <button className='btn btn-primary btn-lg' type='submit'>Search</button>
         </form>
